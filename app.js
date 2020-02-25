e0b931c20c91126f74bb91c1b284fd870b5385af
Jonathan Langner <jonlangner@gmail.com>
2020-02-25 09:36:35 -0600

    Changed comparison to be more strongly typed.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ for(let x = 0; x < friends.length; x++){
     console.log(friends[x] + ": ");
     for(let y = 99; y > 0; y--){    
         
-        if(y == 1){
+        if(y === 1){
             console.log(y + " lines of code in the file, " + y + 
             " lines of code; " + friends[x] + 
             " strikes one out, clears it all out, no more lines of code in the file" )
